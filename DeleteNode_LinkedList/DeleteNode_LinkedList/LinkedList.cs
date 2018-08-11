46dc8166d34a754ed550aac1c0d8be238b51aa19
Adam Forestier <aforestier10@gmail.com>
2018-08-10 21:48:26 -0700

    Delete a user specified node in a linked list

---
+++ DeleteNode_LinkedList/DeleteNode_LinkedList/LinkedList.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeleteNode_LinkedList
+{
+    /// <summary>
+    ///  Create Linked List class
+    /// </summary>
+    class LinkedList
+    {
+        /// <summary>
+        /// Create Node Class
+        /// </summary>
+        public class Node
+        {
+            private Node next;
+            private object data;
+
+            public Node(object data, Node next)
+            {
+                this.data = data;
+                this.next = next;
+            }
+
+            public Node Next
+            {
+                get => next;
+                set => next = value;
+            }
+
+            public object Data
+            {
+                get => data;
+                set => data = value;
+            }
+        }
+
+        private Node head;
+        private int count;
+
+        public LinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public bool Empty
+        {
+            get => count == 0;
+        }
+
+        public int Count
+        {
+            get => count;
+        }
+
+        /// <summary>
+        /// Method to allow user to add objects and specify index
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public object Add(int index, object obj)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index: " + index);
+            }
+
+            if (index > count)
+            {
+                index = count;
+            }
+
+            Node current = head;
+
+            if (Empty || index == 0)
+            {
+                head = new Node(obj, head);
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                current.Next = new Node(obj, current.Next);
+            }
+            count++;
+            return obj;
+        }
+
+        /// <summary>
+        /// Method to allow user to add object without specifying index; appends object to end of list
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public object Add(object obj)
+        {
+            return Add(count, obj);
+        }
+
+        /// <summary>
+        /// Allow user to see entire linked list printed out
+        /// This will verify whether or not item was successfully deleted
+        /// </summary>
+        public void PrintLinkedList()
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// Set current node to head
+        /// Loop until current = null, increasing current by one each iteration
+        /// If current node's data = object. Set current data to next node's data
+        /// Set the next node, to the next next node
+        /// </summary>
+        /// <param name="obj"></param>
+        public void DeleteNode(object obj)
+        {
+            Node current = head;
+
+            while (current != null)
+            {
+                if (current.Data.Equals(obj))
+                {
+                    current.Data = current.Next.Data;
+                    current.Next = current.Next.Next;
+                }
+                current = current.Next;
+            }
+        } 
+    }
+}
---
+++ DeleteNode_LinkedList/DeleteNode_LinkedList/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/// <summary>
+/// Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
+/// 
+/// Problem Notes:
+///     The linked list will have at least two elements
+///     All of the nodes' values will be unique
+///     The given node will not be the tail and it will always be a valid node of the linked list
+///     Do not return anything from your function
+/// 
+/// Example: 
+///     Given linked list -- head = [4,5,1,9], which looks like following: 4 -> 5 -> 1 -> 9
+///     Input: head = [4,5,1,9], node = 5
+///     Output: [4,1,9]
+///     Explanation: You are given the second node with value 5, the linked list should become 4 -> 1 -> 9 after calling your function.
+///     
+/// My Note - Linked List kept in seperate class file "LinkedList.cs"
+/// </summary>
+
+namespace DeleteNode_LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // test input
+            LinkedList list = new LinkedList();
+            list.Add("Test0");
+            list.Add("Test1");
+            list.Add("Test2");
+            list.Add("Test3");
+            list.Add("Test4");
+
+            // show current linked list
+            list.PrintLinkedList();
+
+            // call method to delete
+            list.DeleteNode("Test1");
+            Console.WriteLine("-----");
+
+            // show new list
+            list.PrintLinkedList();
+
+            // keep console open
+            Console.Read();
+        }
+    }
+}
