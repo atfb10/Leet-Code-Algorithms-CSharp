c6c608d0e4147ff3bd474a68493fc508edc7cb59
Adam Forestier <aforestier10@gmail.com>
2018-08-20 11:30:46 -0500

    Return middle node of a singly linked list

---
+++ Middle_Linked_List/Middle_Linked_List/LinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Middle_Linked_List
+{
+    // linked list class
+    public class LinkedList
+    {
+        // node class
+        public class Node
+        {
+            public Node next;
+            public object data;
+
+            public Node(object data, Node next)
+            {
+                this.next = next;
+                this.data = data;
+            }
+
+            public object Data
+            {
+                get => data;
+                set => data = value;
+            }
+
+            public Node Next
+            {
+                get => next;
+                set => next = value;
+            }
+        }
+
+        public Node head;
+        public int count;
+
+        public LinkedList()
+        {
+            head = null;
+            count = 0;
+        }
+
+        public bool Empty
+        {
+            get => count == 0;
+        }
+
+        public int Count
+        {
+            get => count;
+        }
+
+        // Allow user to add node and specify index
+        public object Add(int index, object obj)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException("Index: " + index);
+            }
+
+            if (index > count)
+            {
+                index = count;
+            }
+
+            Node current = head;
+
+            if (Empty || index == 0)
+            {
+                head = new Node(obj, head);
+            }
+            else
+            {
+                for (int i = 0; i < index - 1; i++)
+                {
+                    current = current.Next;
+                }
+                current.Next = new Node(obj, current.Next);
+            }
+            count++;
+            return obj;
+        }
+
+        // Allow addition of node by appending node to end of list
+        public object Add(object obj)
+        {
+            return Add(count, obj);
+        }
+
+        // print a linked list
+        public void Print()
+        {
+            Node current = head;
+
+            while(current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+
+        /// <summary>
+        /// Check if there are zero nodes
+        /// Create two nodes, one that goes through list at double speed and one that goes at normal
+        /// Move through list until faster node reaches null, then return normal node
+        /// </summary>
+        /// <returns></returns>
+        public object FindMid()
+        {
+            if (head == null) return head;
+
+            Node middle = head;
+            Node doubleSpeed = head;
+
+            while (doubleSpeed != null && doubleSpeed.Next != null)
+            {
+                middle = middle.Next;
+                doubleSpeed = doubleSpeed.Next.Next;
+            }
+            return middle.Data;
+        }
+    }
+}
---
+++ Middle_Linked_List/Middle_Linked_List/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Middle_Linked_List
+{
+    /// <summary>
+    /// Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+    /// If there are two middle nodes, return the second middle node.
+    /// 
+    /// Example:
+    /// Input: [1,2,3,4,5]
+    /// Output: Node 3 from this list 
+    /// The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+    /// Note that we returned a ListNode object ans, such that:
+    /// ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+    /// 
+    /// Example: 
+    /// Input: [1,2,3,4,5,6]
+    /// Output: Node 4 from this list
+    /// Since the list has two middle nodes with values 3 and 4, we return the second one.
+    /// 
+    /// Note: All linked list code in class file LinkedList.cs
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // input
+            LinkedList list = new LinkedList();
+            list.Add(33);
+            list.Add(11);
+            list.Add(2);
+            list.Add(3434);
+
+            // call method, pass in arg
+            Console.WriteLine(list.FindMid());
+
+            // keep console open
+            Console.Read();
+        }
+    }
+}
